010b4693f1ae11ae595a310b9f03604553c4e8da
chandra-98a <chandrakalaa2022@gmail.com>
2023-02-09 18:35:38 +0530

    "Executing Array List, Linked List, and Vector "

---
+++ src/com/capgemini/lIst/ArrayList1.java
@@ -0,0 +1,19 @@
+package com.capgemini.lIst;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
+public class ArrayList1{
+    public static void main(String args[]){
+        LinkedList<String> al=new LinkedList<String>();
+        al.add("Ravi");
+        al.add("Vijay");
+        al.add("Ravi");
+        al.add("Ajay");
+        Iterator<String> itr=al.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}
